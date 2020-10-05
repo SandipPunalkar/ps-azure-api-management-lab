27cd380c8d2f04ba1327c3f862f0b8960ed9ff94
Gill Cleeren <gill@snowball.be>
2020-10-05 23:35:37 +0200

    dddd

--- installation.sh
+++ installation.sh
@@ -1,33 +1,36 @@
-
 # variable configuration
-NTSAPINAME=NTSTrafficInfoAPI$(openssl rand -hex 5)
-RES_GROUP=$(az group list --query "[0].name" -o tsv)
+ntsApiName=NTSTrafficInfoAPI$(openssl rand -hex 5)
+RESOURCE_GROUP=$(az group list --query "[0].name" -o tsv)
+PLAN_NAME=myPlan
 DEPLOY_USER="randomUser1$(openssl rand -hex 5)"
 DEPLOY_PASSWORD="Pw1$(openssl rand -hex 10)"
 REMOTE_NAME=production
-GIT_USER=randomgitName
-GIT_EMAIL=random@test.com
-DEPLOY_URL="https://$DEPLOY_USER@$NTSAPINAME.scm.azurewebsites.net/$NTSAPINAME.git"
 
 # Git configuration
+GIT_USERNAME=randomgitName
+GIT_EMAIL=random@test.com
 
-git config --global user.name "$GIT_USER"
+git config --global user.name "$GIT_USERNAME"
 git config --global user.email "$GIT_EMAIL"
 
 # Azure Resource Creation
 
-az appservice plan create --name $NTSAPINAME --resource-group $RES_GROUP --location centralus --sku FREE --verbose
-az webapp create --name $NTSAPINAME --resource-group $RES_GROUP --plan $NTSAPINAME --deployment-local-git --verbose
+az appservice plan create --name $ntsApiName --resource-group $RESOURCE_GROUP --location centralus --sku FREE --verbose
+
+az webapp create --name $ntsApiName --resource-group $RESOURCE_GROUP --plan $ntsApiName --deployment-local-git --verbose
+
 az webapp deployment user set --user-name $DEPLOY_USER --password $DEPLOY_PASSWORD --verbose
 
-# Deployment of app to App Service 
+GIT_URL="https://$DEPLOY_USER@$ntsApiName.scm.azurewebsites.net/$ntsApiName.git"
 
-cd ps-azure-api-management-lab
+git remote add $REMOTE_NAME $GIT_URL
 
-git remote add $REMOTE_NAME $DEPLOY_URL
 git add .
-git commit -m "Initial"
-git push "https://$DEPLOY_USER:$DEPLOY_PASSWORD@$NTSAPINAME.scm.azurewebsites.net/$NTSAPINAME.git" 
+git commit -m "temp"
+git push "https://$DEPLOY_USER:$DEPLOY_PASSWORD@$ntsApiName.scm.azurewebsites.net/$ntsApiName.git" origin main:master
 
-printf "Swagger UI Test page: https://$NTSAPINAME.azurewebsites.net/swagger\n\n"
-printf "Swagger JSON URL. Copy for later: https://$NTSAPINAME.azurewebsites.net/swagger/v1/swagger.json\n\n"
+printf "Swagger URL: https://$ntsApiName.azurewebsites.net/swagger\n"
+
+printf "Example URL: https://$ntsApiName.azurewebsites.net/swagger/v1/swagger.json\n\n"
+
+printf "test"
